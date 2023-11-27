4601bcc7ffdfee9e6a39e7e2e72481f4c8d2f82c
Michael Costa <michaelgm.costa@gmail.com>
2023-11-27 11:05:59 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RockInRio());
+}
+
+class RockInRio extends StatelessWidget {
+  const RockInRio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rock in Rio',
+      theme: ThemeData(appBarTheme: const AppBarTheme(color: Colors.indigo)),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Atrações',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: listAtracoes.length,
+          itemBuilder: (context, index) {
+            return ListTile(title: Text(listAtracoes[index].nome));
+          }),
+    );
+  }
+}
+
+class Atracao {
+  final String nome;
+  final int dia;
+  final List<String> tags;
+
+  const Atracao(this.nome, this.dia, this.tags);
+}
+
+const listAtracoes = [
+  Atracao("Iron Maden", 2, ["Espetaculo", "Fas", "NovoAlbum"]),
+  Atracao("Alok", 3, ["Influente", "Top", "Show"]),
+  Atracao("Justin Bieber", 4, ["TopCharts", "Hits", "PríncipeDoPOP"]),
+  Atracao("Guns N’ Roses", 8, ["Sucesso", "Espetáculo", "Fas"]),
+  Atracao("Capital Inicial", 9, ["2019", "Novo Álbum", "Fas"]),
+  Atracao("Green Day", 9, ["Sucesso", "Reconhecimento", "Show"]),
+  Atracao("Cold Play", 10, ["NovoAlbum", "Sucesso", "2011"]),
+  Atracao("Ivete Sangalo", 10, ["Unica", "Carreiras", "Fas"]),
+  Atracao("Racionais", 3, ["Hits", "Prêmios", "Respeito"]),
+  Atracao("Gloria Groove", 8, ["Streams", "Representatividade", "Sucesso"]),
+  Atracao("Avril Lavigne", 9, ["Estreia", "Sucesso", "Lançamento"]),
+  Atracao("Ludmilla", 10, ["Representativade", "Sucesso", "Parcerias"]),
+];
